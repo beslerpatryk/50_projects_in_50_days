77c815a679041ef130141de06074c4705f25c904
beslerpatryk <beslerpatryk@gmail.com>
2021-10-08 23:36:52 +0800

    feat: further development

--- 02_progress_steps/script.js
+++ 02_progress_steps/script.js
@@ -2,8 +2,15 @@ const progress = document.getElementById('progress')
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
 const circles = document.querySelectorAll('.circle')
+const formEl = document.querySelector("form")
+
+console.log(formEl)
 
 let currentActive = 1
+
+formEl.querySelector(".password").classList.add('hidden')
+formEl.querySelector(".email").classList.add('hidden')
+formEl.querySelector("#submit").classList.add('hidden')
 
 next.addEventListener('click', () =>{
     currentActive++
@@ -46,4 +53,26 @@ function update() {
         prev.disabled = false
         next.disabled = false
     }
+
+    if(currentActive == 1){
+        formEl.querySelector(".username").classList.remove('hidden')
+        formEl.querySelector(".password").classList.add('hidden')
+        formEl.querySelector(".email").classList.add('hidden')
+        formEl.querySelector("#submit").classList.add('hidden')
+    } else if(currentActive == 2){
+        formEl.querySelector(".username").classList.add('hidden')
+        formEl.querySelector(".password").classList.remove('hidden')
+        formEl.querySelector(".email").classList.add('hidden')
+        formEl.querySelector("#submit").classList.add('hidden')
+    } else if(currentActive == 3){
+        formEl.querySelector(".username").classList.add('hidden')
+        formEl.querySelector(".password").classList.add('hidden')
+        formEl.querySelector(".email").classList.remove('hidden')
+        formEl.querySelector("#submit").classList.add('hidden')
+    } else if(currentActive == 4){
+        formEl.querySelector(".username").classList.add('hidden')
+        formEl.querySelector(".password").classList.add('hidden')
+        formEl.querySelector(".email").classList.add('hidden')
+        formEl.querySelector("#submit").classList.remove('hidden')
+    } 
 }

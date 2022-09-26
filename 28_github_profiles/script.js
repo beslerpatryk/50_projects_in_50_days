a6235508dbe6c4612f0020c90a4d1767c01a5e12
beslerpatryk <beslerpatryk@gmail.com>
2022-09-26 13:59:15 +0200

    add: github profiles project

---
+++ 28_github_profiles/script.js
@@ -0,0 +1,23 @@
+const APIURL = 'https://api.github.com/users/'
+const form = document.querySelector('.user-form')
+const search = document.querySelector('.search-box')
+
+async function getUser(username){
+    try {
+        const { data } = await axios(APIURL + username)
+        console.log(data)
+    } catch(err){
+        console.log(err)
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const user = search.value
+
+    if(user) {
+        getUser(user);
+
+        search.value = ''
+    }
+})

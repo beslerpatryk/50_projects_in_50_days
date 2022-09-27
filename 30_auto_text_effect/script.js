925fe875b17dd494b91fd42574a2168ee98af82b
beslerpatryk <beslerpatryk@gmail.com>
2022-09-27 10:38:07 +0200

    add: auto text effect project

---
+++ 30_auto_text_effect/script.js
@@ -0,0 +1,19 @@
+const textEl = document.querySelector('.auto-text')
+const speedEl = document.querySelector('#speed-input')
+const text = "We love programming!"
+let idx = 1
+let speed = 300 / speedEl.value
+
+writeText()
+
+function writeText(){
+    textEl.innerText = text.slice(0,idx)
+    idx++
+    if(idx > text.length){
+        idx=1
+    }
+
+    setTimeout(writeText, speed)
+}
+
+speedEl.addEventListener('input', (e) => speed= 300 / e.target.value)

62861c9f48828cff15cc2f5a2b9f3a90199b236a
beslerpatryk <beslerpatryk@gmail.com>
2022-10-01 11:49:54 +0200

    feat: initial commit

--- 32_cheap_fast_checkboxes/script.js
+++ 32_cheap_fast_checkboxes/script.js
@@ -12,3 +12,51 @@ function doTheTrick(theClickedOne) {
         if(cheap === theClickedOne ) fast.checked = false;
     }
 }
+
+// function foo(){   
+//     function bar() {   
+//              return 3;    
+//     }    
+    
+//     return bar();   
+             
+//     function bar() { 
+//                 return 8;  
+//     }
+// }
+
+// alert(foo());
+
+
+// function parent() {   
+//     var hoisted = "I'm a variable";   
+//     function hoisted() {      
+//         return "I'm a function";   
+//     }    
+    
+//     return hoisted(); 
+// }
+
+// console.log(parent());
+
+
+// alert(foo());
+// function foo() {  
+//     var bar = function() {    
+//         return 3;  
+//     };  
+    
+//     return bar();  
+    
+//     var bar = function() {    
+//         return 8;  
+//     };
+// }
+
+// var myVar = 'foo';
+
+// (function() { 
+//      console.log('Original value was: ' + myVar);  
+//      var myVar = 'bar';  
+//      console.log('New value is: ' + myVar);
+// })();

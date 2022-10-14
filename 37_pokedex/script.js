ef3bc388c98f36b6bc01f9c835130e2974e13ec9
beslerpatryk <beslerpatryk@gmail.com>
2022-10-14 11:11:56 +0200

    feat: fetch pokemons

---
+++ 37_pokedex/script.js
@@ -0,0 +1,33 @@
+const pokeContainer = document.querySelector(".poke-container")
+const pokeCount = 150;
+const colors = {
+    fire: "#FDDFDF",
+    grass: "#DEFDE0",
+    electric: "#FCF7DE",
+    water: "#DEF3FD",
+    ground: "#F4E7DA",
+    rock: "#D5D5D4",
+    fairy: "#FCEAFF",
+    poison: "#98D7A5",
+    bug: "#F8D5A3",
+    dragon: "#97B3E6",
+    psychic: "#EAEDA1",
+    flying: "#F5F5F5",
+    fighting: "#E6E0D4",
+    normal: "#F5F5F5",
+}
+
+const fetchPokemons = async () => {
+    for(let i = 1; i <= pokeCount; i++){
+        await getPokemon(i)
+    }
+}
+
+const getPokemon = async (id) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${id}`
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data)
+}
+
+fetchPokemons()

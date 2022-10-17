80ac82cd20377ca6d8871b055385fc09e5101ac6
beslerpatryk <beslerpatryk@gmail.com>
2022-10-17 12:18:14 +0200

    feat: add API call and Pokemon card  injection

--- 37_pokedex/script.js
+++ 37_pokedex/script.js
@@ -19,7 +19,8 @@ const colors = {
 
 const fetchPokemons = async () => {
     for(let i = 1; i <= pokeCount; i++){
-        await getPokemon(i)
+        let pokemon = await getPokemon(i)
+        createCard(pokemon)
     }
 }
 
@@ -27,7 +28,30 @@ const getPokemon = async (id) => {
     const url = `https://pokeapi.co/api/v2/pokemon/${id}`
     const res = await fetch(url)
     const data = await res.json()
-    console.log(data)
+    // console.log(data)
+    return data
 }
+
+function createCard(pokemon){
+    const card = document.createElement('div')
+
+    card.innerHTML = 
+    `
+    <div class="pokemon" style="background-color: ${colors[pokemon.types[0].type.name]};">
+            <div class="img-container">
+                <img src="${pokemon.sprites.front_default}" alt="${pokemon.name}">
+            </div>
+            <div class="info">
+                <span class="number">#${pokemon.id.toString().padStart(3, '0')}</span>
+                <h3 class="name">${pokemon.name[0].toUpperCase() + pokemon.name.slice(1)}</h3>
+                <small class="type">Type: <span>${pokemon.types[0].type.name}</span></small>
+            </div>
+        </div>
+    `
+
+    pokeContainer.append(card)
+}
+
+
 
 fetchPokemons()

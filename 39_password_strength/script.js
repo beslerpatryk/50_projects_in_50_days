ecbca2c4d1090f1e995b712abd295003a542ed1e
beslerpatryk <beslerpatryk@gmail.com>
2022-10-21 21:07:51 +0200

    feat: blur script

---
+++ 39_password_strength/script.js
@@ -0,0 +1,11 @@
+const password = document.getElementById("password")
+const background = document.getElementById("background")
+let strength = 0;
+
+password.addEventListener('input', (e) => {
+    const input = e.target.value
+    const length = input.length
+    const blurValue = 20 - length*2
+    background.style.filter = `blur(${blurValue}px)`
+})
+
